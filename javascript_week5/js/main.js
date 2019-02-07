6a2926df2f7eb72c93a2f0a68c89969110202769
visualstudio <visualstudio@230-PC02>
2019-02-07 11:44:34 -0500

    array in javascript

--- javascript_week5/js/main.js
+++ javascript_week5/js/main.js
@@ -1,1 +1,11 @@
 ﻿// Your code here!
+function myFunction(a, b) {
+    
+    return a+b;//This function returns the sum of a and b
+}
+
+function arrays() {
+    var pens;
+    pens = ["red", "orange", "green", "blue"];
+    console.log(pens);
+}
